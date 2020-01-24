18e4aa0cdd9888ca2af27d7916cbbb893306309d
rvchauhan <ravi.chauhan@successive.tech>
2020-01-24 13:43:44 +0530

    refactor:Diamond and Equilateral file

--- extra/patterns/Diamond.js
+++ extra/patterns/Diamond.js
@@ -1,36 +1,31 @@
-let r=Number(process.argv[2]);
-diamond(r);
+let r = Number (process.argv[2]) ;
+diamond(r) ;
 function diamond(n)
 {
- if(n<11 && n>1)
- {
-   for(let i=0;i<n;i++)
-   {
-     let b =" ";
-     for(let j=0;j<n-i;j++)
-     {
-       b=b+" ";
-     }
-     for(let k=0;k<=i;k++)
-     {
-       b=b+"* ";
-     }
-     console.log(b);
-   }
-   for(let i=n-1;i>0;i--)
-   {
-     let b =" ";
-     for(let j=0;j<n-i;j++)
-     {
-       b=b+" ";
-     }
-     for(let k=0;k<i;k++)
-     {
-       b=b+" *";
-     }
-     console.log(b);    }
+ if (n<11 && n>1)
+  {
+        for ( let i = 0; i < n; i++ )
+        {
+             let b = " " ;
+        for ( let j = 0;j <  n-i; j++)
+            b= b + " ";
+        for ( let k = 0; k <= i; k++ )
+            b = b + "* " ;
+     
+        console.log (b) ;
+         }
+      for ( let i = n-1; i > 0; i--)
+       {
+              let b = " " ;
+             for ( let j = 0;j < n-i; j++)
+             b = b + " " ;
+              for ( let k = 0; k < i; k++ )
+            b = b + " *" ;
+        console.log(b);  
+        }
  }
- else{
+ else  
+ {
      console.log("wrong input");
  }
 }
