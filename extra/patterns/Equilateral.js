cb3eee6ac65bfcbe72c8fe29bd321aa579ced399
rvchauhan <ravi.chauhan@successive.tech>
2020-01-27 15:46:01 +0530

    Refactor:  Diamond.js & Equilateral

--- extra/patterns/Equilateral.js
+++ extra/patterns/Equilateral.js
@@ -1,30 +1,24 @@
+
 let n = Number(process.argv[2]);
 equilateral(n);
-function equilateral(b)
-{
+function equilateral(b) {
     //let n = prompt("enter value of n");
-    if(b > 2 && b < 11)
-    {
-for(let i = 1;i <= b;i++)
-{
-    let s = "";
- for(let j = 1;j <= b-i; j++)
- {
- s=s + " ";
- }
- for(let k = 0; k < 2 * i - 1;k++)
- {
-  if(k % 2 == 0)
-    s = s + "*";
-    else
-    s = s + " "
- }
- console.log(s);
- }
+    if (b > 2 && b < 11) {
+        for (let i = 1; i <= b; i++) {
+            let s = "";
+            for (let j = 1; j <= b - i; j++) {
+                s = s + " ";
+            }
+            for (let k = 0; k < 2 * i - 1; k++) {
+                if (k % 2 == 0)
+                    s = s + "*";
+                else
+                    s = s + " "
+            }
+            console.log(s);
+        }
+    } else {
+        console.log("wrong choice");
+    }
 }
-else
-{
-    console.log("wrong choice");
-}
- }
- 
+
