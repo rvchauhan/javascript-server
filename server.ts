98e1c93cb03b5f5b465a4f89c0089a955320cb3f
rvchauhan <ravi.chauhan@successive.tech>
2020-01-30 18:46:21 +0530

    feat: add Handlers and middleware

---
+++ server.ts
@@ -0,0 +1,77 @@
+import { server } from "typescript";
+import * as express from 'express';
+import * as bodyParser from 'body-parser';
+import { Request } from 'express';
+import notFoundRoutes from './src/libs/routes/notFoundRoute'
+import errorHandler from './src/libs/routes/errorHandler'
+
+interface User 
+{
+    id: string;
+    name: string;
+}
+
+interface NewRequest extends Request 
+{
+    user: User;
+}
+
+class Server {
+    private app: express.Application;
+    constructor(private config)
+     {
+        this.app = express();
+     }
+    bootstrap = () =>
+     {
+        this.initBodyParser();
+        this.setupRoutes();
+        return this;
+     }
+
+    run = () =>
+     {
+        const { app, config: { port } } = this;
+        app.listen(port, (err) => {
+            if (err) {
+                throw err;
+            }
+            console.log(`App is running successfuly on port ${port}`);
+
+        })
+    }
+    initBodyParser = () =>
+     {
+        const { app } = this;
+        app.use(bodyParser.urlencoded({ extended: false }))
+
+        // parse application/json
+        app.use(bodyParser.json())
+
+     }
+    setupRoutes = () => 
+    {
+
+        const { app } = this;
+        app.use('/health-check', (req, res) => 
+        {
+            console.log('Inside health check');
+            res.send('I am OK');
+        });
+
+        app.use('/api', (req: NewRequest, res, next) => 
+        {
+            console.log("inside Middleware");
+            req.user = {
+                id: '1',
+                name: 'Node'
+            }
+            res.send('OK')
+        })
+
+        app.use(notFoundRoutes);
+        app.use(errorHandler);
+        return this;
+    }
+}
+export default Server;
--- src/Server.ts
+++ src/Server.ts
@@ -1,38 +1,77 @@
 import { server } from "typescript";
 import * as express from 'express';
+import * as bodyParser from 'body-parser';
+import { Request } from 'express';
+import notFoundRoutes from './libs/routes/notFoundRoute'
+import errorHandler from './libs/routes/errorHandler'
 
-class Server 
+interface User 
 {
+    id: string;
+    name: string;
+}
+
+interface NewRequest extends Request 
+{
+    user: User;
+}
+
+class Server {
     private app: express.Application;
     constructor(private config)
      {
         this.app = express();
      }
-    bootstrap = () => 
-    {
-    this.setupRoutes();
-    return this;
+    bootstrap = () =>
+     {
+        this.initBodyParser();
+        this.setupRoutes();
+        return this;
+     }
+
+    run = () =>
+     {
+        const { app, config: { port } } = this;
+        app.listen(port, (err) => {
+            if (err) {
+                throw err;
+            }
+            console.log(`App is running successfuly on port ${port}`);
+
+        })
     }
+    initBodyParser = () =>
+     {
+        const { app } = this;
+        app.use(bodyParser.urlencoded({ extended: false }))
 
-run = () => {
-    const { app, config: { port } } = this;
-    app.listen(port, (err) => {
-        if (err) {
-            throw err;
-        }
-        console.log(`App is running successfuly on port ${port}`);
+        // parse application/json
+        app.use(bodyParser.json())
 
-    })
-}
-setupRoutes = () => 
-{
+     }
+    setupRoutes = () => 
+    {
 
-    const { app } = this;
-    app.use('/health-check', (req, res) => {
-        console.log('Inside health check');
-        res.send('I am OK');
-    });
-    return this;
-}
+        const { app } = this;
+        app.use('/health-check', (req, res) => 
+        {
+            console.log('Inside health check');
+            res.send('I am OK');
+        });
+
+        app.use('/api', (req: NewRequest, res, next) => 
+        {
+            console.log("inside Middleware");
+            req.user = {
+                id: '1',
+                name: 'Node'
+            }
+            res.send('OK')
+        })
+
+        app.use(notFoundRoutes);
+        app.use(errorHandler);
+        return this;
+    }
 }
 export default Server;
