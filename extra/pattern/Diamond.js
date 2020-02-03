7b4c0af039a852351cf8f0b54b52eb5b7d30eb30
rvchauhan <ravi.chauhan@successive.tech>
2020-02-03 17:10:40 +0530

    fixed indentations

--- extra/pattern/Diamond.js
+++ extra/pattern/Diamond.js
@@ -1,14 +1,14 @@
 function diamond(n) {
-     if ( n < 11 && n > 1){
-        for ( let i = 0; i < n; i++ ) {
+    if (n < 11 && n > 1) {
+        for (let i = 0; i < n; i++) {
             let b = " ";
-            for ( let j = 0; j < n - i; j++ )
+            for (let j = 0; j < n - i; j++)
                 b = b + " ";
-            for ( let k = 0; k <= i; k++ )
+            for (let k = 0; k <= i; k++)
                 b = b + "* ";
             console.log(b);
         }
-       for ( let i = n - 1; i > 0; i-- ) {
+        for (let i = n - 1; i > 0; i--) {
             let b = " ";
             for (let j = 0; j < n - i; j++)
                 b = b + " ";
@@ -17,7 +17,7 @@ function diamond(n) {
             console.log(b);
         }
     } else {
-        console.log( " wrong input " );
+        console.log(" wrong input ");
     }
-} 
+}
 export default diamond; 
