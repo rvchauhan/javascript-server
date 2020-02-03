be8cf3b9e1b7fa3d8ce110815cc6852c0b96f96a
rvchauhan <ravi.chauhan@successive.tech>
2020-02-03 12:54:33 +0530

    Fixed merge conflicts

--- extra/utils/helper.js
+++ extra/utils/helper.js
@@ -3,8 +3,4 @@ function validateEmail(b)
     const regex = /([a-zA-z0-9\+_.@])+@successive.tech/gm;
     return regex.test(b);
 }
-<<<<<<< HEAD
 export default validateEmail;
-=======
-    export default validateEmail;
->>>>>>> 9c27c621cc46d85ab3093d622e94ab7ca137c74e
