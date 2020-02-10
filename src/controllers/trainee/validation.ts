9a73145ab7e64340043b111d272d5dfc4d3808cb
rvchauhan <ravi.chauhan@successive.tech>
2020-02-10 15:59:31 +0530

    refactor: error status code and validation file

--- src/controllers/trainee/validation.ts
+++ src/controllers/trainee/validation.ts
@@ -1,70 +1,70 @@
 
 export default {
-create:
-{
-id:
-{
-required: true,
-string: true,
-in: ['body'],
-// tslint:disable-next-line:object-literal-shorthand
-custom: (value: number) => {
-console.log('Value', value);
-}
-},
-name:
-{
-required: true,
-regex: /^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/,
-in: ['body'],
-errorMessage: 'Name is required',
-}
-},
-delete:
-{
-id:
-{
-required: true,
-errorMessage: 'Id is required',
-in: ['params']
-}
-},
-get:
-{
-skip:
-{
-required: false,
-default: 0,
-number: true,
-in: ['query'],
-errorMessage: 'Skip is invalid',
-},
-limit:
-{
-required: false,
-default: 10,
-number: true,
-in: ['query'],
-errorMessage: 'Limit is invalid',
-}
-},
-update:
-{
-id:
-{
-required: true,
-string: true,
-in: ['body']
-},
-dataToUpdate:
-{
-in: ['body'],
-required: true,
-isObject: true,
-// tslint:disable-next-line:object-literal-shorthand
-custom: (dataToUpdate: any) => {
-console.log(dataToUpdate);
-},
-}
-}
+  create:
+  {
+    id:
+    {
+      required: true,
+      string: true,
+      in: ['body'],
+      // tslint:disable-next-line:object-literal-shorthand
+      custom: (value: number) => {
+        console.log('Value', value);
+      }
+    },
+    name:
+    {
+      required: true,
+      regex: /^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/,
+      in: ['body'],
+      errorMessage: 'Name is required',
+    }
+  },
+  delete:
+  {
+    id:
+    {
+      required: true,
+      errorMessage: 'Id is required',
+      in: ['params']
+    }
+  },
+  get:
+  {
+    skip:
+    {
+      required: false,
+      default: 0,
+      number: true,
+      in: ['query'],
+      errorMessage: 'Skip is invalid',
+    },
+    limit:
+    {
+      required: false,
+      default: 10,
+      number: true,
+      in: ['query'],
+      errorMessage: 'Limit is invalid',
+    }
+  },
+  update:
+  {
+    id:
+    {
+      required: true,
+      string: true,
+      in: ['body']
+    },
+    dataToUpdate:
+    {
+      in: ['body'],
+      required: true,
+      isObject: true,
+      // tslint:disable-next-line:object-literal-shorthand
+      custom: (dataToUpdate: any) => {
+        console.log(dataToUpdate);
+      },
+    }
+  }
 };
