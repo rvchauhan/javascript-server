eb26e74561023a04cb79772879040866d7ae9ba0
rvchauhan <ravi.chauhan@successive.tech>
2020-02-10 15:41:10 +0530

    refactor: error status code and validation file

--- src/controllers/trainee/validation.ts
+++ src/controllers/trainee/validation.ts
@@ -11,22 +11,22 @@ const validation = {
           error: "Id is required",
           message: "Id is required",
           timestamp: new Date(),
-          status: 500,
+          status: 403,
         },
         typeError: {
           error: "ID and name should be of string type",
           message: "ID and name should be of string type",
           timestamp: new Date(),
-          status: 500,
+          status: 403,
         }
       },
       custom: function (value) {
         console.log('Value', value);
-        if(!value){
-        throw {
-          error: 'Error Occured', message: 'Message'
+        if (!value) {
+          throw {
+            error: 'Error Occured', message: 'Message'
+          }
         }
-      }
       }
     },
     name: {
@@ -38,13 +38,13 @@ const validation = {
           error: "Name is required",
           message: "Name is required",
           timestamp: new Date(),
-          status: 500,
+          status: 403,
         },
         regexError: {
           error: "Regex did not match",
           message: "Regex did not match",
           timestamp: new Date(),
-          status: 500,
+          status: 403,
         }
       }
     }
@@ -74,7 +74,7 @@ const validation = {
           error: "Skip should be of type number",
           message: "Skip should be of type number",
           timestamp: new Date(),
-          status: 500,
+          status: 403,
         }
       }
     },
@@ -88,7 +88,7 @@ const validation = {
           error: "Limit should be of type number",
           message: "Limit should be of type number",
           timestamp: new Date(),
-          status: 500,
+          status: 403,
         }
       }
     }
@@ -103,13 +103,13 @@ const validation = {
           error: "Id is required",
           message: "Id is required",
           timestamp: new Date(),
-          status: 500,
+          status: 403,
         },
         typeError: {
           error: "ID should be of string type and dataToUpadte should be of type object",
           message: "ID should be of string type and dataToUpadte should be of type object",
           timestamp: new Date(),
-          status: 500,
+          status: 403,
         }
       }
     },
@@ -117,27 +117,27 @@ const validation = {
       in: ['body'],
       required: true,
       isObject: true,
-      errorMessage: { 
+      errorMessage: {
         Error: {
           error: "dataToUpdate is required",
           message: "dataToUpdate is required",
           timestamp: new Date(),
-          status: 500,
+          status: 403,
         },
         typeError: {
           error: "ID should be of string type and dataToUpadte should be of type object",
           message: "ID should be of string type and dataToUpadte should be of type object",
           timestamp: new Date(),
-          status: 500,
+          status: 403,
         }
       },
       custom: function (dataToUpdate) {
         console.log('Value', dataToUpdate);
-        if(!dataToUpdate){
-        throw {
-          error: 'Error Occured', message: 'Message'
+        if (!dataToUpdate) {
+          throw {
+            error: 'Error Occured', message: 'Message'
+          }
         }
-      }
       },
     }
   }
