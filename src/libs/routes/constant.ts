cbe38115f4d72e722265b884f5fba68b7d12e6ec
rvchauhan <ravi.chauhan@successive.tech>
2020-02-11 12:43:21 +0530

    refactor:constant.ts

--- src/libs/routes/constant.ts
+++ src/libs/routes/constant.ts
@@ -2,23 +2,9 @@ import { Ipermissions, Iusers } from '../../../extraTs/interfaces';
 const permissions: Ipermissions = {
     'getUsers': {
         all: ['head-trainer'],
-        read: ['trainee', 'trainer'],
-        write: ['trainer'],
-        delete: [],
+        read: ['trainee', 'trainer','head-trainer'],
+        write: ['trainer','head-trainer'],
+        delete: ['head-trainer'],
     }
 };
-let users: Iusers[] = [{
-    traineeEmail: "trainee1@successive.tech",
-    reviewerEmail: "reviewer1@successive.tech"
-},
-{
-    traineeEmail: "trainee2@successiveriv.tech",
-    reviewerEmail: "reviewer2@successive.tech"
-},
-{
-    traineeEmail: "trainee3@successive.tech",
-    reviewerEmail: "reviewer3@successive.tech"
-}
-]
-
 export default permissions;
