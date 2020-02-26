e96cca3c0147da771ef4c23023cf46c6181b22c1
rvchauhan <ravi.chauhan@successive.tech>
2020-02-26 12:41:28 +0530

    fix : pull from develop

--- src/controllers/trainee/routes.ts
+++ src/controllers/trainee/routes.ts
@@ -3,15 +3,12 @@ import { Router } from 'express';
 import validation from './validation';
 import validationHandler from '../../libs/routes/validationHandler'
 import authmiddleware from './../../libs/routes/authmiddleware'
-import UserRouter from '../user/routes';
 const traineeRouter = Router();
 //console.log(validation.create);
 traineeRouter.route('/trainee')
-UserRouter.route('/user')
   .get(authmiddleware('getUsers', 'write'), validationHandler(validation.get), TraineeController.create)
   .post(authmiddleware('getUsers', 'write'), validationHandler(validation.create), TraineeController.list)
-  .put(authmiddleware('getUsers', 'write'), validationHandler(validation.update), TraineeController.put)
-traineeRouter.delete('/trainee/:id', authmiddleware('getUsers', 'write'), validationHandler(validation.delete), TraineeController.delete);
-UserRouter.route('/me').get(authmiddleware('getUsers', 'read'), validationHandler(validation.get), TraineeController.create)
-export default traineeRouter;
+  .put( authmiddleware('getUsers', 'write'),validationHandler(validation.update), TraineeController.put)
+traineeRouter.delete('/trainee/:id', authmiddleware('getUsers', 'write'),validationHandler(validation.delete), TraineeController.delete);
 
+export default traineeRouter;
--- src/controllers/user/Controller.ts
+++ src/controllers/user/Controller.ts
@@ -5,6 +5,8 @@ import IRequest from '../../libs/routes/IRequest'
 import config from '../../config/configuration'
 import * as bcrypt from 'bcrypt'
 import * as jwt from 'jsonwebtoken'
+import Iusercreate from './../../repositories/entities/Iusercreate'
+import seedData from '../../libs/seedData'
 import { JsonWebTokenError } from 'jsonwebtoken';
 class UserController {
     static instance: UserController;
@@ -17,13 +19,18 @@ class UserController {
         UserController.instance = new UserController();
         return UserController.instance;
     }
-    create = (req: Request, res: Response, next: NextFunction) => {
+
+    create = async (req: Request, res: Response, next: NextFunction) => {
         try {
             console.log('-----------------CREATE TRAINEE USER----------------:');
-            const { email, name, address, hobbies, dob, mobile_number } = req.body;
-            this.userRepository.create({
-                email, name, address, dob, mobile_number, hobbies
-            })
+            const users: Iusercreate = req.body;
+            const password = users.password;
+            async function encodedPassword(password) {
+                return await bcrypt.hash(password, 10)
+            }
+            const pass = await encodedPassword(password);
+            Object.assign(users, { password: pass });
+            this.userRepository.create(users)
                 .then(user => {
                     return SystemResponse.success(res, user, 'trainee added sucessfully');
                 }).catch(error => {
@@ -33,45 +40,41 @@ class UserController {
             return next({ error: err, message: err });
         }
     };
-    list = (req: Request, res: Response, next: NextFunction) => {
+
+    list = async (req: Request, res: Response, next: NextFunction) => {
         try {
             console.log('-------------INSIDE LIST TRAINEE----------- ');
-            this.userRepository.list().then(user => {
-                console.log(user);
-                return SystemResponse.success(res, user, 'Users List');
-            }).catch(error => {
-                throw error;
-            });
+            const user = await this.userRepository.list()
+            return SystemResponse.success(res, user, 'Users List');
         }
         catch (err) {
             return next({ error: err, message: err });
         }
     };
-    update = (req: Request, res: Response, next: NextFunction) => {
+
+    update = async (req: Request, res: Response, next: NextFunction) => {
         try {
             console.log('------------INSIDE UPDATE TRAINEE-------------');
             const { id, dataToUpdate } = req.body;
-            this.userRepository.update({ _id: id }, dataToUpdate).then(user => {
-                return SystemResponse.success(res, user, 'Updated user');
-            }).catch(error => {
-                throw error;
-            });
+            const user = await this.userRepository.update({ _id: id }, dataToUpdate)
+            return SystemResponse.success(res, user, 'Updated user');
         }
         catch (err) {
             return next({ error: err, message: err });
         }
     };
-    delete = (req: Request, res: Response, next: NextFunction) => {
+
+    delete = async (req: Request, res: Response, next: NextFunction) => {
         try {
             console.log(' :::::::::: Inside Delete Trainee :::::::: ');
             const { id } = req.params;
-            this.userRepository.delete({ _id: id }).then(user => {
-                return SystemResponse.success(res, user, 'User Deleted Successfully')
-            });
+            const user = await this.userRepository.delete({ _id: id })
+            return SystemResponse.success(res, user, 'User Deleted Successfully')
         } catch (err) {
             throw err;
         }
     };
+
     me = async (req: IRequest, res: Response, next: NextFunction) => {
         try {
             console.log(":::::::::::::::INSIDE ME::::::::::::::");
@@ -82,32 +85,28 @@ class UserController {
             return next({ error: err, message: err });
         }
     }
-    login = (req: IRequest, res: Response, next: NextFunction) => {
+    
+    login = async (req: IRequest, res: Response, next: NextFunction) => {
         try {
             console.log("::::::::::::INSIDE LOG IN::::::::::::");
             const { email, password } = req.body;
-            this.userRepository.findone(email).then(user => {
-                const match = bcrypt.compare(user.password, password);
-                if (match) {
-                    const token = jwt.sign({ id: user.id, email: user.email, exp: Math.floor(Date.now() / 1000) + 15 * 60 }, config.secretKey)
-                    if (!token) {
-                        console.log("token is not generated")
-                    } else {
-                        return SystemResponse.success(res, token, "token generated")
-                    }
-                    return SystemResponse.success(res, req.user, " Logged in");
+            const user = await this.userRepository.findone(email)
+            const match = await bcrypt.compare(password, user.password);
+            if (match) {
+                const token = await jwt.sign({ id: user.id, email: user.email, exp: Math.floor(Date.now() / 1000) + 15 * 60 }, config.secretKey)
+                if (!token) {
+                    console.log("token is not generated")
                 } else {
-                    console.log("not a User")
+                    return SystemResponse.success(res, token, "token generated")
                 }
-            })
+                return SystemResponse.success(res, req.user, " Logged in");
+            } else {
+                console.log("not a User")
+            }
         }
         catch (err) {
             throw err;
         }
     }
-
-
-
 }
-
 export default UserController.getInstance();
